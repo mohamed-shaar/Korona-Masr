0a8af363733bbd6522c579bb8c5f89681444360a
NOURHAN JAMAL <nourhanjamalx@gmail.com>
2020-04-11 20:46:48 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HospitalsComponent } from './hospitals/hospitals.component';
+import { NumbersComponent } from './numbers/numbers.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HospitalsComponent,
+    NumbersComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule
